34e603614bb42ead2f597d58ab463d22493699ed
naomi <naom0649@stud.kea.dk>
2021-10-01 09:19:25 +0200

    opsat github

---
+++ src/AdventureGame.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class AdventureGame {
+
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        // opret alle de 9 rum først room r1 = new room
+
+      /*  String Room1;
+        String Room2;
+        String Room3;
+        String Room4;
+        String Room5;
+        String Room6;
+        String Room7;
+        String Room8;
+        String Roome9;
+*/
+
+    }
+    public void AdventureGame (){
+        Room Room1 = new Room();
+
+        System.out.println(" ");
+
+
+
+
+
+
+
+    }
+
+
+
+    }
+
---
+++ src/Room.java
@@ -0,0 +1,19 @@
+public class Room {
+
+    public class Direction{
+
+        private String north;
+        private String south;
+        private String east;
+        private String west;
+
+
+
+
+    }
+
+    String exit;
+    String help;
+    String look;
+
+}
